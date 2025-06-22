3e0664e01bb7a819eaa51eb7404cbc1481e0bddf
Smrutishree <smrutishree.naik123@gmail.com>
2025-06-22 22:39:41 +0530

    added register view

--- newsfeedapp/newsfeed/urls.py
+++ newsfeedapp/newsfeed/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', views.news_create, name='news_create'),
     path('<int:news_id>/edit/', views.news_edit, name='news_edit'),
     path('<int:news_id>/delete/', views.news_delete, name='news_delete'),
+    path('register/', views.register, name='register'),
 ] 
--- newsfeedapp/newsfeed/views.py
+++ newsfeedapp/newsfeed/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import News
-from .forms import NewsForm
+from .forms import NewsForm, UserRegistrationForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 def index(request):
     return render(request, 'index.html')
@@ -10,6 +12,7 @@ def news_list(request):
     newsList = News.objects.all().order_by('-created_at')
     return render(request, 'news_list.html', {'newsList':newsList})
 
+@login_required
 def news_create(request):
     if request.method == "POST":
         form = NewsForm(request.POST, request.FILES)
@@ -22,6 +25,7 @@ def news_create(request):
         form = NewsForm()
     return render(request, 'news_form.html', {'form':form})
 
+@login_required
 def  news_edit(request, news_id):
     news = get_object_or_404(News, pk=news_id, user = request.user)
     if request.method == "POST":
@@ -35,9 +39,23 @@ def  news_edit(request, news_id):
         form = NewsForm(instance=news)
     return render(request, 'news_form.html', {'form':form})
     
+@login_required
 def news_delete(request, news_id):
     news = get_object_or_404(News, pk=news_id, user = request.user)
     if request.method == "POST":
         news.delete()
         return redirect('news_list')
     return render(request, 'news_confirm_delete.html', {'news':news})
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('news_list')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form':form})
